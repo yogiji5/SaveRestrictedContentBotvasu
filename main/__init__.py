b5152a647d922da9b3f997536d8020be49770baf
yogiji5 <gimopo.l562@gmail.com>
2024-10-24 04:57:16 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,41 +1,43 @@
+#Github.com/Vasusen-code
+
 from pyrogram import Client
+
+from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
+
+from decouple import config
 import logging, time, sys
 
-# Set up logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-# Variables
-API_ID = 23080322
-API_HASH = 'b3611c291bf82d917637d61e4a136535'
-BOT_TOKEN = '7096314428:AAG-JSo5pi2VTU-i1bNSsKk_Jkjt2dsZoBE'
-SESSION = 'BAFgLYIAPwR4P_tICKd2uf_etLU02a2_hu8bWsFaMRjqM9rtNVj28wUUCXdSWC7bRUpQZhcwf0BULgg7LIjiZpHGJXCXqA9EUAVl02DZavQhWqJaB2IilNdgqIwsXOw1huHuL3dUU2vqf4vDXzLvOrTZvbvfi5gJk9oLd3f7ycbm2zK0CRHpvkTzN_iWCqnjgbZRWSI6L9OG4R6qeKM0K_QUyAcQlP2SwI7vEwHgCdIwhgh_3_rmUdESx-2fpj5-4heI8hnNw4nDpUGXrBSdHrqkMBCBh1aVQEdVWrAyJ-e_HyCn1Twe-4El3RQ-sh7rFxIfSM5G8zZEKDph_DXTkstu1JFB7QAAAAGCggAWAA'
-FORCESUB = 'Bhupendra_Jogi_Enodes'
-AUTH = 5500868603
+# variables
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None)
+AUTH = config("AUTH", default=None, cast=int)
 
-# Initialize the bot
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID)
+userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
-# Start userbot
 try:
     userbot.start()
-except Exception as e:
-    print(f"Userbot Error: {e}")
+except BaseException:
+    print("Userbot Error ! Have you added SESSION while deploying??")
     sys.exit(1)
 
-# Initialize and start the main bot
 Bot = Client(
     "SaveRestricted",
     bot_token=BOT_TOKEN,
-    api_id=API_ID,
+    api_id=int(API_ID),
     api_hash=API_HASH
-)
+)    
 
 try:
     Bot.start()
 except Exception as e:
-    print(f"Bot Error: {e}")
+    print(e)
     sys.exit(1)
